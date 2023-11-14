ff9f3bbe22d04f697af3d661449e746309119003
PetersWalker <petersinclairwalker@gmail.com>
2023-11-14 15:37:17 -0500

    publish stats to server

--- main.go
+++ main.go
@@ -5,25 +5,25 @@ import (
 	"log"
 	"net/http"
 	"encoding/json"
-	"os"
+	"io"
 )
 
 func main() {
-	http.HandleFunc("/", handler) // each request calls handler
-	http.HandleFunc("/printStats", printStatsHandler)
+	http.HandleFunc("/", healthhandler) // each request calls handler
+	http.HandleFunc("/postStats", postStatsHandler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 // handler echoes the Path component of the request URL r.
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+func healthhandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "healthy")
 }
 
-func printStatsHandler(w http.ResponseWriter, r *http.Request) {
+func postStatsHandler(w http.ResponseWriter, r *http.Request) {
 	var data map[string]string
 	err:= json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
 		fmt.Fprint(w, err)
 	}
-	fmt.Fprint(os.Stdout, data)
+	fmt.Printf("%v\n", data)
 }
--- wrapper.go
+++ wrapper.go
@@ -5,8 +5,10 @@ import (
 	"os/exec"
 	"fmt"
 	"os"
+	"net/http"
+	"bytes"
 	"strings"
-	// "bytes"
+	"encoding/json"
 )
 
 func main() {
@@ -49,15 +51,49 @@ func collectCommitStats() {
 	output, err := exec.Command("git", "diff").Output()
 
 	if err != nil {
-		log.Fatal("collectCommiStats", err)
+		log.Fatal("collectCommitStats", err)
 	}
 	str := strings.Split(string(output), "\n")
 
 	// possibly some input validation here on the git diff bytes?
 	// e.g. if not valit log.Fatal("non standard git diff format %v", str(output))
+	for _, v := range str {
+		fmt.Println(v)
+	}
 	diffLines := str[4]
 	subtracted := diffLines[6:8]
 	added := diffLines[12:14]
 
-	fmt.Printf("%v, %v", added, subtracted)
+	type Stats struct {
+		LinesAdded string
+		LinesSubtracted string
+	}
+
+	stats := Stats{
+		LinesAdded: added,
+		LinesSubtracted: subtracted,
+	}
+
+	b, err := json.Marshal(stats)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body := bytes.NewBuffer(b)
+	req, err := http.NewRequest("POST", "http://localhost:8000/postStats", body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	if res.StatusCode != 200 {
+		panic(res.Status)
+	}
 }
