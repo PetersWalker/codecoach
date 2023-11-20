72831aceaf5e8ec555c4142fdb5bdffdffce68d1
PetersWalker <petersinclairwalker@gmail.com>
2023-11-20 12:49:16 -0500

    feat: html template skeleton

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"codecoach/handlers"
 	"database/sql"
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
@@ -12,10 +13,19 @@ import (
 var db *sql.DB
 
 func main() {
-	http.HandleFunc("/", healthhandler)
+	counter := 0
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("public/index.html"))
+		tmpl.Execute(w, counter)
+		counter = counter + 1
+	}
+
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/health", healthhandler)
 	http.HandleFunc("/postStats", handlers.PostStatsHandler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 	fmt.Println("listening on local:8000")
+
 }
 
 // handler echoes the Path component of the request URL r.
